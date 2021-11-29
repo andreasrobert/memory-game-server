efad89b7b1d8503895356b9cc246582884c3a4c3
andreasrobert <andreasrobb23@gmail.com>
2021-11-30 01:34:27 +0700

    fix player from client

--- player.go
+++ player.go
@@ -49,18 +49,18 @@ func newPlayer(conn *websocket.Conn, wsServer *WsServer, name string) *Player {
 
 }
 
-func (client *Player) readPump() {
+func (player *Player) readPump() {
 	defer func() {
-		client.disconnect()
+		player.disconnect()
 	}()
 
-	client.conn.SetReadLimit(maxMessageSize)
-	client.conn.SetReadDeadline(time.Now().Add(pongWait))
-	client.conn.SetPongHandler(func(string) error { client.conn.SetReadDeadline(time.Now().Add(pongWait)); return nil })
+	player.conn.SetReadLimit(maxMessageSize)
+	player.conn.SetReadDeadline(time.Now().Add(pongWait))
+	player.conn.SetPongHandler(func(string) error { player.conn.SetReadDeadline(time.Now().Add(pongWait)); return nil })
 
-	// Start endless read loop, waiting for messages from client
+	// Start endless read loop, waiting for messages from player
 	for {
-		_, jsonMessage, err := client.conn.ReadMessage()
+		_, jsonMessage, err := player.conn.ReadMessage()
 		if err != nil {
 			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
 				log.Printf("unexpected close error: %v", err)
@@ -68,53 +68,53 @@ func (client *Player) readPump() {
 			break
 		}
 
-		client.handleNewMessage(jsonMessage)
+		player.handleNewMessage(jsonMessage)
 	}
 
 }
 
-func (client *Player) writePump() {
+func (player *Player) writePump() {
 	ticker := time.NewTicker(pingPeriod)
 	defer func() {
 		ticker.Stop()
-		client.conn.Close()
+		player.conn.Close()
 	}()
 	for {
 		select {
-		case message, ok := <-client.send:
-			client.conn.SetWriteDeadline(time.Now().Add(writeWait))
+		case message, ok := <-player.send:
+			player.conn.SetWriteDeadline(time.Now().Add(writeWait))
 			if !ok {
 				// The WsServer closed the channel.
-				client.conn.WriteMessage(websocket.CloseMessage, []byte{})
+				player.conn.WriteMessage(websocket.CloseMessage, []byte{})
 				return
 			}
 
-			w, err := client.conn.NextWriter(websocket.TextMessage)
+			w, err := player.conn.NextWriter(websocket.TextMessage)
 			if err != nil {
 				return
 			}
 			w.Write(message)
 
 			// Attach queued chat messages to the current websocket message.
-			n := len(client.send)
+			n := len(player.send)
 			for i := 0; i < n; i++ {
 				w.Write(newline)
-				w.Write(<-client.send)
+				w.Write(<-player.send)
 			}
 
 			if err := w.Close(); err != nil {
 				return
 			}
 		case <-ticker.C:
-			client.conn.SetWriteDeadline(time.Now().Add(writeWait))
-			if err := client.conn.WriteMessage(websocket.PingMessage, nil); err != nil {
+			player.conn.SetWriteDeadline(time.Now().Add(writeWait))
+			if err := player.conn.WriteMessage(websocket.PingMessage, nil); err != nil {
 				return
 			}
 		}
 	}
 }
 
-func (client *Player) disconnect() {
+func (player *Player) disconnect() {
 	
 }
 
@@ -123,43 +123,43 @@ func ServeWs(wsServer *WsServer, w http.ResponseWriter, r *http.Request) {
 
 }
 
-func (client *Player) handleNewMessage(jsonMessage []byte) {
+func (player *Player) handleNewMessage(jsonMessage []byte) {
 
 
 }
 
-func (client *Player) handleJoinRoomMessage(message Message) {
+func (player *Player) handleJoinRoomMessage(message Message) {
 	roomName := message.Message
 
-	client.joinRoom(roomName, nil)
+	player.joinRoom(roomName, nil)
 }
 
-func (client *Player) handleLeaveRoomMessage(message Message) {
-
-}
-
-
-func (client *Player) joinRoom(roomName string, sender *Player) {
+func (player *Player) handleLeaveRoomMessage(message Message) {
 
 }
 
-func (client *Player) isInRoom(room *Room) bool {
-	if _, ok := client.rooms[room]; ok {
+
+func (player *Player) joinRoom(roomName string, sender *Player) {
+
+}
+
+func (player *Player) isInRoom(room *Room) bool {
+	if _, ok := player.rooms[room]; ok {
 		return true
 	}
 
 	return false
 }
 
-func (client *Player) notifyRoomJoined(room *Room, sender *Player) {
+func (player *Player) notifyRoomJoined(room *Room, sender *Player) {
 	message := Message{
 		Action: RoomJoinedAction,
 		Target: room,
 	}
 
-	client.send <- message.encode()
+	player.send <- message.encode()
 }
 
-func (client *Player) GetName() string {
-	return client.Name
+func (player *Player) GetName() string {
+	return player.Name
 }
